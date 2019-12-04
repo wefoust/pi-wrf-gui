d42d999e9cae870a6c65ce014462e92a17e4c93b
wefoust <43735499+wefoust@users.noreply.github.com>
2019-12-04 13:26:27 -0700

    fix typo

--- src/pi-wrf/Pages/start_page.py
+++ src/pi-wrf/Pages/start_page.py
@@ -52,7 +52,7 @@ class StartPage(tk.Frame):
         if check_internet():
             btn_1.config(command=lambda : [controller.show_frame])
         else:
-            btn_1.config(command=lambda : [messagebox.showwarning("Warning", "No network connection detected. cannot run live simulation. Please exit application and check connection."))])                      
+            btn_1.config(command=lambda : [messagebox.showwarning("Warning", "No network connection detected. cannot run live simulation. Please exit application and check connection.")])                      
         btn_2 = tk.Button(self,
                           text="Run Archived Simulation",
                           font=("Arial Bold",40),
